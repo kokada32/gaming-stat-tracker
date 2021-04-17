c42f4cc2b80c681e668b38705b599587a59ed81e
Kenny Okada <kokada32@gmail.com>
2021-04-17 16:10:40 -0700

    imported react-router-dom and added nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import MatchList from './pages/matchList';
+import { Switch, Route, Link } from 'react-router-dom';
+import matchList from './pages/matchList';
 
 function App() {
   return (
     <div>
-      <MatchList />
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to={""}>Home</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <Link className="dropdown-item" to={""}>Most Recent</Link>
+            <Link className="dropdown-item" to={""}>Most Kills</Link>
+            <Link className="dropdown-item" to={""}>Most Damage</Link>
+        </div>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={matchList} />
+        </Switch>
+      </main>
     </div>
   );
 }
