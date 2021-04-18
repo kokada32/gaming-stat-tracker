6ac3bdd4ad5ecbdaa47536e0096305d89602f20a
Kenny Okada <kokada32@gmail.com>
2021-04-17 17:41:48 -0700

    connected sort functions to links on matchlist.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
             <Link className="navbar-brand" to={""}>Home</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <Link className="dropdown-item" to={""}>Most Recent</Link>
-            <Link className="dropdown-item" to={""}>Most Kills</Link>
-            <Link className="dropdown-item" to={""}>Most Damage</Link>
         </div>
       </nav>
       <main>
--- src/pages/matchList.js
+++ src/pages/matchList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MatchRow from '../components/matchRow';
 import matches from '../matches.json';
 
@@ -6,10 +7,28 @@ class matchList extends Component {
     state = {
         matches
     }
+
+    sortByMostRecent = () => {
+        console.log("most recent");
+    }
+
+    sortByMostKills = () => {
+        console.log("most kills");
+    }
+
+    sortByMostDamage = () => {
+        console.log("most damage");
+    }
+
     render() {
         const allMatchesRows = matches.map( m => <MatchRow key={m.matchId} {...m} />);
         return (
             <div>
+                <section className="jumbotron">
+                    <Link className="dropdown-item" to={""} onClick={this.sortByMostRecent}>Most Recent</Link>
+                    <Link className="dropdown-item" to={""} onClick={this.sortByMostKills}>Most Kills</Link>
+                    <Link className="dropdown-item" to={""} onClick={this.sortByMostDamage}>Most Damage</Link>
+                </section>
                 <div className="container">
                     <div className="row row-cols-2">
                         {allMatchesRows}
