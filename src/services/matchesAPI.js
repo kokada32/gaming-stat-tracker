9b4bd0876a86c86ef85c45f3a8e795eb00dcb442
Kenny Okada <kokada32@gmail.com>
2021-04-21 19:53:26 -0700

    updated authheaders in matchesAPI.js

--- src/services/matchesAPI.js
+++ src/services/matchesAPI.js
@@ -1,12 +1,21 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const baseUrl = "https://parseapi.back4app.com/classes/Match";
 
+const authHeaders = {
+    'X-Parse-Application-Id': process.env.REACT_APP_PARSE_APPLICATION_ID,
+    'X-Parse-Javascript-Key': process.env.REACT_APP_PARSE_JAVASCRIPT_KEY
+}
+
 const index = () => {
-    const matches = fetch(baseUrl);
-    return matches;
+    return fetch(baseUrl, { headers: authHeaders })
+        .then( response => response.json());
 }
 
 const MatchesAPI = {
-
+    index
 };
 
 export default MatchesAPI;
