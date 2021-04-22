4462b670f42de83386a2c570a971d8ee046e1875
Kenny Okada <kokada32@gmail.com>
2021-04-22 14:39:49 -0700

    added css to match card on matchForm and matchRow

--- src/components/matchForm.js
+++ src/components/matchForm.js
@@ -25,79 +25,79 @@ const MatchForm = ({match, setMatch, buttonText, cancelPath}) => {
 
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label>
-                <span>Game Type</span>
-                <input
-                    className="form-control"
-                    name="gameType"
-                    defaultValue={""}
-                    placeholder="Solos, Duos, Trios, Quads"
-                    onChange={handleChange}
-                />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                <span>Kills</span>
-                <input
-                    className="form-control"
-                    name="kills"
-                    defaultValue={0}
-                    placeholder="Number"
-                    onChange={handleChange}
-                />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                <span>Deaths</span>
-                <input
-                    className="form-control"
-                    name="deaths"
-                    defaultValue={0}
-                    onChange={handleChange}
-                />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                <span>Damage</span>
-                <input
-                    className="form-control"
-                    name="damage"
-                    defaultValue={0}
-                    onChange={handleChange}
-                />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                <span>Date</span>
-                <input
-                    className="form-control"
-                    name="date"
-                    defaultValue={""}
-                    onChange={handleChange}
-                />
-                </label>
-            </div>
-            <div className="form-group">
-                <label>
-                <span>Placement</span>
-                <input
-                    className="form-control"
-                    name="placement"
-                    defaultValue={""}
-                    onChange={handleChange}
-                />
-                </label>
-            </div>
-            <button className="btn btn-primary">{buttonText}</button>
-            <Link className="btn btn-link" to={cancelPath}>Cancel</Link>
-        </form>
-    </div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>
+                    <span>Game Type</span>
+                    <input
+                        className="form-control"
+                        name="gameType"
+                        defaultValue={""}
+                        placeholder="Solos, Duos, Trios, Quads"
+                        onChange={handleChange}
+                    />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                    <span>Kills</span>
+                    <input
+                        className="form-control"
+                        name="kills"
+                        defaultValue={0}
+                        placeholder="Number"
+                        onChange={handleChange}
+                    />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                    <span>Deaths</span>
+                    <input
+                        className="form-control"
+                        name="deaths"
+                        defaultValue={0}
+                        onChange={handleChange}
+                    />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                    <span>Damage</span>
+                    <input
+                        className="form-control"
+                        name="damage"
+                        defaultValue={0}
+                        onChange={handleChange}
+                    />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                    <span>Date</span>
+                    <input
+                        className="form-control"
+                        name="date"
+                        defaultValue={""}
+                        onChange={handleChange}
+                    />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                    <span>Placement</span>
+                    <input
+                        className="form-control"
+                        name="placement"
+                        defaultValue={""}
+                        onChange={handleChange}
+                    />
+                    </label>
+                </div>
+                <button className="btn btn-primary">{buttonText}</button>
+                <Link className="btn btn-link" to={cancelPath}>Cancel</Link>
+            </form>
+        </div>
     );
 }
 
--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -1,14 +1,18 @@
 const matchRow = ({gameType, kills, deaths, damage, date, placement}) => {
     return (
-        <div className="col">
+        <div>
             <div className="card">
-                <div className="card-body">
-                    <div className="card-title">{gameType}</div>
-                    <div className="card-text">Kills: {kills}</div>
-                    <div className="card-text">Deaths: {deaths}</div>
-                    <div className="card-text">Damage: {damage}</div>
-                    <div className="card-text">Date: {date}</div>
-                    <div className="card-text">Placement: {placement}</div>
+                <div className="card-body matchlist-grid-container">
+                    <div className="grid-item grid-gameType">{gameType}</div>
+                    <div className="grid-item grid-placement">{placement}</div>
+                    <div className="grid-item grid-date">{date}</div>
+                    <div className="grid-space"></div>
+                    <div className="grid-item grid-kills-label">Kills:</div> 
+                    <div className="grid-item grid-kills">{kills}</div>
+                    <div className="grid-item grid-deaths-label">Deaths:</div>
+                    <div className="grid-item grid-deaths">{deaths}</div>
+                    <div className="grid-item grid-damage-label">Damage:</div>
+                    <div className="grid-item grid-damage">{damage}</div>
                 </div>
             </div>
         </div>
