201778ee44a47d81838586ccf22ded3a742c6792
Kenny Okada <kokada32@gmail.com>
2021-04-22 12:51:22 -0700

    added css to navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import matchList from './pages/matchList';
 import matchNew from './pages/matchNew';
+import nukeImage from './nuke.jpg';
 
 function App() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <Link className="navbar-brand" to={""}>Warzone Game Tracker</Link>
-        </div>
+    <div id="app">
+      <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-between" id="navBar">
+          <Link className="navbar-brand" to={"/"}><img src={nukeImage} alt={"nuke"} id="nukeImage"/></Link>
+          <h1 id="appTitle">Warzone Stat Tracker</h1>
+          <Link className="btn btn-primary btn-lg" to={"/match/new"} id="newMatchButton">New Match</Link>
       </nav>
       <main>
         <Switch>
